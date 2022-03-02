25f1a9b0ef1a744aef598f0da4b9b16ff5339025
fraihan-dw <fraihan964@gmail.com>
2022-03-02 22:55:14 +0700

    memperbaiki fungsi task1, menambahkan fungsi task2

--- 6_Branching - Looping - Function/Praktikum/task1.dart
+++ 6_Branching - Looping - Function/Praktikum/task1.dart
@@ -5,28 +5,20 @@ Jika nilai > 0 return C
 Return teks kosong
 */
 import 'dart:io';
-String scoreA(double nilai){
-  return "A";
+String score(double nilai){
+  if (nilai > 70) {
+    return "A";
+  } else if (nilai > 40) {
+    return "B";
+  } else if (nilai > 0) {
+    return "C";
+  } else {
+    return "";
+  }
 }
-String scoreB(double nilai){
-  return "B";
-}
-String scoreC(double nilai){
-  return "C";
-}
-String kosong(double nilai){
-  return "";
-}
+
 void main() {
   stdout.write("Masukan nilai anda: ");
   double nilai = double.parse(stdin.readLineSync()!);
-  if (nilai > 70) {
-    print(scoreA(nilai));
-  } else if (nilai > 40) {
-    print(scoreB(nilai));
-  } else if (nilai > 0) {
-    print(scoreC(nilai));
-  } else {
-    print(kosong(nilai));
-  }
+  print("Maka grade nilai anda adalah ${score(nilai)}");
 }
--- 6_Branching - Looping - Function/Praktikum/task2.dart
+++ 6_Branching - Looping - Function/Praktikum/task2.dart
@@ -1,5 +1,4 @@
-import 'dart:io';
-void main() {
+/* codingan awal void main() {
   int faktorialisasi = 1;
   stdout.write("Masukkan bilangan bulat : ");
   int? nilai = int.parse(stdin.readLineSync()!);
@@ -11,4 +10,17 @@ void main() {
     }
     print("Hasil faktorial dari $nilai adalah $faktorialisasi");
   }
+}*/
+// codingan setelah direvisi
+defacto(int value){
+  int faktorialisasi = 1;
+  for (int i = 1; i <= value; i++){
+     faktorialisasi *= i;
+   }
+  return faktorialisasi;
+}
+void main() {
+  print("Maka Faktorial dari 10 adalah ${defacto(10)}");
+  print("Maka Faktorial dari 20 adalah ${defacto(20)}");
+  print("Maka Faktorial dari 30 adalah ${defacto(30)}");
 }
