cfab805b6bf6b2b33b56a0f9c01408d8744da089
fraihan-dw <fraihan964@gmail.com>
2022-03-07 15:21:08 +0700

    Memperbaiki logic map, nambah function

--- 7_Advance Funtion - Async-Await/Praktikum/task2.dart
+++ 7_Advance Funtion - Async-Await/Praktikum/task2.dart
@@ -1,10 +1,16 @@
+dataMap (md){
+  Map<String, String> mdmp = {};
+  for(var dat in md){
+    mdmp[dat[0]] = dat[1];
+  }
+  print(mdmp);
+}
 void main(){
-  List<int> list = [10, 3, 4, 1, 7, 2];
-  List<String> nmap = [];
-  print(list);
-
-  nmap = list.map((coba) => "angka " + coba.toString()).toList();
-  nmap.forEach((element) {
-    print(element);
-  });
+  var md = [
+    ["id", "327540"],
+    ["tipe keanggotaan", "Diamond"],
+    ["nama", "Raihan"],
+  ];
+  print(md);
+  dataMap(md);
 }
