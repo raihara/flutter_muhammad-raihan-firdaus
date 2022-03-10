f860039959176fada8999cc474117913fb195324
fraihan-dw <fraihan964@gmail.com>
2022-03-10 18:07:37 +0700

    tambah method tambahmuatan, penyesuain totalmuatan

--- 9_Dart Object Oriented Programming 1/Praktikum/task2.dart
+++ 9_Dart Object Oriented Programming 1/Praktikum/task2.dart
@@ -1,14 +1,43 @@
-import 'task1.dart';
-class mobil{
-  totalMuatan(){
-    var call = Hewan();
-    var nilai = Mobil();
-    var total = call.beratAyam + nilai.kapasitas;
-    print(total);
+class Hewan {
+  int? berat;
+}
+
+class Mobil {
+  int overload = 400;
+  int kapasitas = 0;
+  List<Hewan> muatan = [];
+  int? tambahMuatan (Hewan animal) {
+    int total = animal.berat!;
+    var i = 0;
+    while (i < muatan.length){
+      total = total + muatan[i].berat!;
+      i++;
+    }    
+    if (total <= overload){
+      muatan.add(animal);
+      kapasitas = total;
+      print('berat kapasitas: $kapasitas kg');
+    }else{
+      print("Overload");
+    }
+  }
+  int TotalMuatan(){
+    return muatan.length;
   }
 }
 
 void main(){
-  var call = mobil();
-  call.totalMuatan();
+  var called = Mobil();
+  var sapi = Hewan();
+  var kambing = Hewan();
+  var unta = Hewan();
+  sapi.berat = 50;
+  kambing.berat = 20;
+  unta.berat = 70;
+  called.tambahMuatan(sapi);
+  print('Total muatan: ${called.TotalMuatan()}');
+  called.tambahMuatan(kambing);
+  print('Total muatan: ${called.TotalMuatan()}');
+  called.tambahMuatan(unta);
+  print('Total muatan: ${called.TotalMuatan()}');
 }
