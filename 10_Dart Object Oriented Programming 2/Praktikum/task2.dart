51a1ecd6062570c2e2b21218b9c389d7a7d537e4
fraihan-dw <fraihan964@gmail.com>
2022-03-14 17:30:58 +0700

    pisah intruksi no.1, 2, jadi isinya intruksi no.2

--- 10_Dart Object Oriented Programming 2/Praktikum/task2.dart
+++ 10_Dart Object Oriented Programming 2/Praktikum/task2.dart
@@ -1,32 +1,8 @@
-//Soal Task intruksi no.3 dan 4 pada slide 33 section 10
-import 'dart:math';
-class Matematika{
-  var c, sisa;
-  hasil(){
-    if(x<y){
-      c=x;
-      x=y;
-      y=c;
-    }
-    do{
-      sisa = x%y;
-      x=y;
-      y=sisa;
-    }while(sisa !=0);
-    print(x);
-
-  }
-}
-class KelipatanPersekutuanTerkecil implements Matematika{
-  x = 10;
-  y = 5;
-  @override
-  hasil() {
-    return hasil();
-  }
-}
-
+//Soal Task intruksi no.2 pada slide 32 section 10
+import 'task1.dart';
 void main(){
-  var operation = Matematika();
-  print(operation.hasil());
+  var bangun = Balok();
+  print('volume Balok tersebut adalah ${bangun.volume()}');
+  var bangun2 = Kubus();
+  print('Volume kubus adalah ${bangun2.volume()}');
 }
