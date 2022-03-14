282c54f68bc8fe441db746971bad809bb753310f
fraihan-dw <fraihan964@gmail.com>
2022-03-14 17:33:14 +0700

    commit intruksi no.4, main, lainnya import task3

---
+++ 10_Dart Object Oriented Programming 2/Praktikum/task4.dart
@@ -0,0 +1,8 @@
+//Soal Task intruksi no.4 pada slide 33 section 10
+import 'task3.dart';
+void main(){
+  var operation = FaktorPersekutuanTerbesar();
+  print(operation.hasil());
+  var operation2 = KelipatanPersekutuanTerkecil();
+  print(operation2.hasil());
+}
